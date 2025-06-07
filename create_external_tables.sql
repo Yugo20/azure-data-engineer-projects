30f9cc1bbf4dbf6c5f667aeb9d9108aa4c7bc0e4
manish040596 <144101684+manish040596@users.noreply.github.com>
2025-06-07 11:34:45 +0530

    Update create_external_tables.sql

--- create_external_tables.sql
+++ create_external_tables.sql
@@ -1,23 +1,27 @@
+-- sql table
 
--- External table for daily revenue
-CREATE EXTERNAL TABLE gold.daily_revenue (
+CREATE EXTERNAL DATA SOURCE blob_retail_datasource
+WITH (
+    LOCATION = 'https://pocretail.blob.core.windows.net/retail'
+);
+
+
+CREATE EXTERNAL FILE FORMAT ParquetFormat
+WITH (
+    FORMAT_TYPE = PARQUET
+);
+
+CREATE EXTERNAL TABLE daily_revenue (
     event_date DATE,
     daily_revenue FLOAT,
     total_purchases INT
 )
 WITH (
-    LOCATION = 'gold/daily_revenue/',
-    DATA_SOURCE = your_adls_datasource,
+    LOCATION = '/gold/',  -- Important: folder path only
+    DATA_SOURCE = blob_retail_datasource,
     FILE_FORMAT = ParquetFormat
 );
 
--- External table for top product categories
-CREATE EXTERNAL TABLE gold.top_categories (
-    product_category STRING,
-    total_sales FLOAT
-)
-WITH (
-    LOCATION = 'gold/top_categories/',
-    DATA_SOURCE = your_adls_datasource,
-    FILE_FORMAT = ParquetFormat
-);
+
+
+select * from daily_revenue
